4051bb731ca5c16b8b20c5cef88b0bca0ad775c2
jeannegokotano <jeanne.gokotano@flexisourceit.com.au>
2020-07-29 07:49:21 +0800

    Add timestamp in filename

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const downloadsFolder = require('downloads-folder');
 
 
 function snapscreen(url) {
+
+    let now = new Date();
+
     if (!url.includes('https') && !url.includes('http')) {
         url.concat('https://');
     }
+
     try {
         (async() => {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             await page.setViewport({ width: 1920, height: 1080 });
             await page.goto(url);
-            await page.screenshot({ path: downloadsFolder() + '/page.png', fullPage: true }).then(() => {
+            await page.screenshot({ path: downloadsFolder() + '/' + now + '-page.png', fullPage: true }).then(() => {
                 console.log('Image generated. Please check your downloads folder');
             });
             await browser.close();
